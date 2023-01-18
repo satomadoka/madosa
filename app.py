0472eb914f2a544c9de4c506ff02e74c0aeece59
satomadoka <120892863+satomadoka@users.noreply.github.com>
2023-01-18 23:27:22 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.sidebar.title("最適化パラメーター")
 def objective_function(x):
     return x**2 +x
 
-x_min = st.sidebar.number_imput("xの最小値", value=-10.0, step=0.1)
+x_min = st.sidebar.number_input("xの最小値", value=-10.0, step=0.1)
 x_max = st.sidebar.number_input("xの最大値", value=10.0, step=0.1)
 
 algorithm = st.sidebar.selectbox("最適化アルゴリズム", ["BFGS", "L-BFGS-B", "CG", "Newton-CG", "TNC"])
